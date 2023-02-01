adbe68b47ff96eb0c910f69ac54bd36d40c7befd
Tom Zimmerman <you@example.com>
2023-02-01 13:49:19 -0500

    completed application

---
+++ prj-00-initial-starting-setup/src/components/base-component.ts
@@ -0,0 +1,46 @@
+
+  //Component Base Class
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+
+      //This is a way of importing the content of a DOM elelment specifically on a template element, pretty neat
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+
+      this.element = <U>importedNode.firstElementChild;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBeginning ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+
